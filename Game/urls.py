4dca544b67991762ca0ed50f3e652f2aff098058
eyeri <ricky50869@gmail.com>
2026-03-03 22:15:37 -0800

    Milestone 2 : Sample UI

--- Game/urls.py
+++ Game/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     # TODO (Milestone 3):
     # - Add URL routes for advanced actions (DOUBLE, SPLIT)
     # - Introduce route-level validation once full rule set is implemented
-    path('milestone2_api_demo/', views.milestone2_api_demo, name='api_demo'),
+    path('', views.main_page, name='home'),
 ]
--- Game/views.py
+++ Game/views.py
@@ -11,6 +11,7 @@ from django.urls import path, include
 from django.http import JsonResponse
 from .engine import GameEngine
 from .engine_api import get_view_state
+from django.shortcuts import render
 
 def index(request):
     """
@@ -24,24 +25,5 @@ def index(request):
 
     return render(request, 'blackjack/index.html')
 
-def milestone2_api_demo(request):
-    """
-    PROTOTYPE VALIDATION ONLY:
-    This view proves that the core engine and the API contract (engine_api.py)
-    are functional within the Django framework. 
-    Full session management and UI integration are deferred to Milestone 3.
-    """
-    # Initialize the robust engine
-    engine = GameEngine()
-    engine.new_round() # Executes initial deal
-    
-    # Generate the View State (JSON)
-    # This proves we can communicate game state to a front-end
-    view_data = get_view_state(engine, hide_dealer_hole=True)
-    
-    return JsonResponse({
-        "milestone": 2,
-        "validation_status": "Passed",
-        "logic_verification": "Engine-to-API bridge functional",
-        "game_data": view_data
-    })
+def main_page(request):
+    return render(request, 'UI.html')
